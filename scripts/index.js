1e60a9fb171eaccac5832c14d49a2ac676df9944
NitishSingh <nitishrajput4444@gmail.com>
2024-12-18 01:13:08 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ const authedUser = {
     username: "nitishkumar12",
     online: true,
     lastSeen: "Today",
-    avatar: "/static/nitishrajput.jpg"
+    avatar: "static/chat-placeholder.svg"
 };
 
 // create instance of ChatApp,
